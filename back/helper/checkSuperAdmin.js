c26a3549023f7fc7c12e9eabf64e61d90fbec146
aziz-sassi <sassiaziz50@gmail.com>
2022-06-03 16:31:38 +0100

    live

--- back/helper/checkSuperAdmin.js
+++
@@ -1,9 +0,0 @@
-const checkSuperAdmin = (req, res, next) => {
-  if (req.user && req.user.superAdmin === true) {
-    next()
-  } else {
-    res.status(400).send({ error: "User must be super admin" })
-  }
-}
-
-module.exports = checkSuperAdmin
